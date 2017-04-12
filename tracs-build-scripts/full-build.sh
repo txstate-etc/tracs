9be9f550ce6539f7934dbc4204c113f70e0498ba
Yuanhua Qu <yuanhuaqu@yuanhua.its.txstate.edu>
2017-04-12 13:38:25 -0500

    Updating docker folder in build script

--- tracs-build-scripts/full-build.sh
+++ tracs-build-scripts/full-build.sh
@@ -1,15 +1,15 @@
 REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 echo "Removing files from previous builds..."
-rm -r $REPO_ROOT/docker/components $REPO_ROOT/docker/lib $REPO_ROOT/docker/webapps
-mkdir $REPO_ROOT/docker/lib
+rm -r $REPO_ROOT/tracs-docker/components $REPO_ROOT/tracs-docker/lib $REPO_ROOT/tracs-docker/webapps
+mkdir $REPO_ROOT/tracs-apachedocker/lib
 mkdir -p /tmp/tracs-apache
 echo "Copying core tomcat libraries..."
 wget -qO- http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz | tar -xz --strip-component 1 -C /tmp/tracs-apache
-cp -r /tmp/tracs-apache/lib/* $REPO_ROOT/docker/lib/
+cp -r /tmp/tracs-apache/lib/* $REPO_ROOT/tracs-docker/lib/
 rm -r /tmp/tracs-apache
 
 echo "Starting build..."
 cd $REPO_ROOT
 MAVEN_OPTS="-Xmx1024m -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
-mvn clean install sakai:deploy -Dmaven.tomcat.home=$REPO_ROOT/docker -P mysql
+mvn clean install sakai:deploy -Dmaven.tomcat.home=$REPO_ROOT/tracs-docker -P mysql

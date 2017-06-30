80cad6cf993e1dc146bf059d82c9019c82815292
Anne Faber <anne.faber@txstate.edu>
2017-06-30 10:28:14 -0500

    This is the script I use to build all three tracs skins

---
+++ tracs-build-scripts/build-skins.sh
@@ -0,0 +1,16 @@
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+echo "building skins..."
+
+cd $REPO_ROOT/reference
+
+mvn clean compile  -Dsakai.skin.target=tracs-default -Dsakai.skin.customization.file=./src/morpheus-master/sass/tracs-skins/_tracs-default.scss
+mvn clean compile  -Dsakai.skin.target=tracs-blue -Dsakai.skin.customization.file=./src/morpheus-master/sass/tracs-skins/_tracs-blue.scss
+mvn clean compile  -Dsakai.skin.target=tracs-yellow -Dsakai.skin.customization.file=./src/morpheus-master/sass/tracs-skins/_tracs-yellow.scss
+
+#clear the customizations so that the default sakai skin can be built
+> ./src/morpheus-master/sass/_customization.scss
+
+mvn clean install sakai:deploy -Dmaven.tomcat.home=$REPO_ROOT/tracs-docker/
+
+echo "finished building skins"

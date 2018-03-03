08114385de9783e6ed645f1a8cb346327eb1779e
Yuanhua Qu <yq12@txstate.edu>
2018-03-02 20:03:19 -0600

    Commented out gradebook tool switches until we decide what to do with existing gradebook

--- tracs-scripts/upgrade_scripts/11.3.1.sql
+++ tracs-scripts/upgrade_scripts/11.3.1.sql
@@ -1053,9 +1053,9 @@ ALTER TABLE VALIDATIONACCOUNT_ITEM ADD COLUMN EID VARCHAR(255);
 -- SAK-31468 rename existing gradebooks to 'Gradebook Classic'
 -- This will not change any tool placements. To do that, uncomment the following line:
 -- UPDATE SAKAI_SITE_TOOL SET REGISTRATION='sakai.gradebookng' WHERE REGISTRATION='sakai.gradebook.tool';
-UPDATE SAKAI_SITE_TOOL SET REGISTRATION='sakai.gradebookng' WHERE REGISTRATION='sakai.gradebook.tool';
-UPDATE SAKAI_SITE_TOOL SET TITLE='Gradebook Classic' WHERE TITLE='Gradebook';
-UPDATE SAKAI_SITE_PAGE SET TITLE='Gradebook Classic' WHERE TITLE='Gradebook';
+-- UPDATE SAKAI_SITE_TOOL SET REGISTRATION='sakai.gradebookng' WHERE REGISTRATION='sakai.gradebook.tool';
+-- UPDATE SAKAI_SITE_TOOL SET TITLE='Gradebook Classic' WHERE TITLE='Gradebook';
+-- UPDATE SAKAI_SITE_PAGE SET TITLE='Gradebook Classic' WHERE TITLE='Gradebook';
 
 
 -- SAK-23666/SAK-31603
